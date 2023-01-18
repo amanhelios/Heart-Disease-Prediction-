82be0406a8ce0d6ea555ac61fc263e698e328f6f
amanhelios <96583888+amanhelios@users.noreply.github.com>
2023-01-18 19:51:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+#importing libraries
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+#loading the csv data to a pandas DataFrame
+heart_data = pd.read_csv('heart.csv')
+
+# print first 5 rows of the dataset
+heart_data.head()
+
+# print last 5 rows of the dataset
+heart_data.tail()
+
+# number of rows and colomns in the dataset
+heart_data.shape
+
+# getting some info about the data
+heart_data.info()
+
+# cheking for missing values
+heart_data.isnull().sum()
+
+# statistical measures about the data
+heart_data.describe()
 
