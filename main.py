5e3416ac1d79e2681c1c69a4c694ee3c932844ef
amanhelios <96583888+amanhelios@users.noreply.github.com>
2023-01-19 16:11:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ heart_data['target'].value_counts()
 X = heart_data.drop(columns= 'target', axis=1)
 Y = heart_data['target']
 
+
 print(X)
 
 #splitting the Data into Training data & Test Data
@@ -55,3 +56,29 @@ model.fit(X_train, Y_train)
 # accuracy on training data
 X_train_prediction = model.predict(X_train)
 training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+
+print('Accuracy on Training data :', training_data_accuracy)
+
+# accuracy on test data
+X_test_prediction = model.predict(X_test)
+test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
+
+print('Accuracy on Test data :', test_data_accuracy)
+
+# Building a Predictive System
+
+input_data = (62,0,0,140,268,0,0,160,0,3.6,0,2,2)
+
+# Change the input data to a numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+# Reshape the numpy array as we are predicting for only on instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+prediction = model.predict(input_data_reshaped)
+print(prediction)
+
+if(prediction[0] == 0):
+  print('The Person does not have a Heart Disease')
+else:
+  print('The Person has Heart Disease')
